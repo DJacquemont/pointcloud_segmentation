5dc40f60e641f9b6de6b35e906ab9edf9d225cf7
Dimitri Jacquemont <dimitri.jacquemont@epfl.ch>
2023-10-02 11:37:57 +0200

    Initial commit

--- src/pointcloud_segmentation_node.cpp
+++ src/pointcloud_segmentation_node.cpp
@@ -1,8 +1,36 @@
 #include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <sensor_msgs/PointCloud2.h>
 
+#include <sstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <eigen3/Eigen/Dense>
+
+#include "vector3d.h"
+#include "pointcloud.h"
+#include "hough.h"
+
+// Callback function receiving ToF images from the Autopilot package
+void pointcloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+  ROS_INFO("Pointcloud Received");
+}
+
+// Main function
 int main(int argc, char* argv[]){
 
-    ROS_INFO("This is the pointcloud semgmentation node!");
+    ROS_INFO("Pointcloud semgmentation node starting");
+
+    ros::init(argc, argv, "pointcloud_seg");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber tof_pc_sub = n.subscribe("tof_pc", 1000, pointcloudCallback);
+
+    ros::spin();
 
     return 0;
 }
